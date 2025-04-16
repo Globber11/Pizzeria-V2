6f6c014a38ef13e85434086fab9ca2eacaf7a478
Globber11 <kg05052011@gmail.com>
2025-04-16 18:37:07 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,31 @@
+from random import *
+import json
+
+class Registrator:
+    def crate_new_account(self, name, surname, phone_number, born_date):
+        seed(name + surname + str(phone_number) + str(born_date))
+        ID = int(random() * 10 ** 15)
+        with open('acoounts.json', 'r', encoding='utf-8') as file:
+            content = file.read()
+            if content.strip():
+                users = json.loads(content)
+            else:
+                users = []
+        for user in users:
+            if user['ID'] == ID:
+                return f'Авторизация завершена'
+        account_data = {
+            'ID': ID,
+            'name': name,
+            'last_name': surname,
+            'phone_number': phone_number,
+            'born_year': born_date
+        }
+
+        users.append(account_data)
+        with open('acoounts.json', 'w', encoding='utf-8') as file:
+            json.dump(users, file, ensure_ascii=False, indent=4)
+        return f'Регистрация завершена'
 
 class Pizza:
     def __init__(self, price: int, ingredients: dict[str, int], name: str):
