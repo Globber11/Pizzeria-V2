ba34c43855e524be394dc4f84986a2f96466d874
ViperrIvan <smokerskatedik@gmail.com>
2025-04-16 22:10:19 +0700

    Update view.py

--- view.py
+++ view.py
@@ -1,6 +1,17 @@
+from typing import List
+
+class OrderView:
+    @classmethod
+    def product_selection(cls, product_names: List):
+        print("Выберите продукт:")
+        for _ in range(1,len(product_names)+1):
+            print(f"\t{_}. {product_names[_-1]}")
+
+
 class CheckGenerator:
     def __init__(self, busket: dict[str, list[int, int]]):
         self.__busket = busket
+
     def print_check(self):
         print("\n" + "=" * 30)
         for _ in self.__busket:
