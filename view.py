f826a12c3881e40f86735daa66688b80627d41bb
Globber11 <kg05052011@gmail.com>
2025-04-16 21:23:32 +0700

    Update view.py

--- view.py
+++ view.py
@@ -4,3 +4,6 @@ class CheckGenerator:
     def print_check(self):
         print("\n" + "=" * 30)
         for _ in self.__busket:
+            print(f'{_} {self.__busket[_][1]}шт.')
+        print(f'Total to pay: {sum(self.__busket[_][0] * self.__busket[_][1] for _ in self.__busket)}')
+        print("=" * 30)
