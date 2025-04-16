598ca94101f87069fd17692ba392e0938de396f8
Globber11 <kg05052011@gmail.com>
2025-04-16 23:00:40 +0700

    Update model.py

--- model.py
+++ model.py
@@ -68,7 +68,6 @@ class Pizza(ABC):
     def __str__(self):
         return f"{self._name} (Цена: {self._base_price}, Ингредиенты: {self._ingredients})"
 
-
 class Margarita(Pizza):
     def __init__(self):
         super().__init__(
@@ -76,7 +75,6 @@ class Margarita(Pizza):
             ingredients={"cheese": 100, "tomatoes": 200, "sauce": 50, "dough": 300},
             base_price=450
         )
-
 
 class Pepperoni(Pizza):
     def __init__(self):
@@ -86,7 +84,6 @@ class Pepperoni(Pizza):
             base_price=550
         )
 
-
 class FourCheeses(Pizza):
     def __init__(self):
         super().__init__(
@@ -95,7 +92,6 @@ class FourCheeses(Pizza):
             base_price=600
         )
 
-
 class HamCheese(Pizza):
     def __init__(self):
         super().__init__(
@@ -103,7 +99,6 @@ class HamCheese(Pizza):
             ingredients={"cheese": 150, "ham": 70, "sauce": 50, "dough": 300},
             base_price=500
         )
-
 
 class Hawaiian(Pizza):
     def __init__(self):
@@ -128,3 +123,63 @@ class Busket:
 
     def get_busket(self):
         return self.__busket
+
+class Warehouse:
+    def subtract_products(self, product, quantity):
+        def load():
+            try:
+                with open('products.json', 'r', encoding='utf-8') as file:
+                    content = file.read()
+                    if content.strip():
+                        return json.loads(content)
+                    return []
+            except (FileNotFoundError, json.JSONDecodeError):
+                return []
+        def edit(products_):
+            for _ in products_:
+                if _ == product:
+                    products_[product] -= quantity
+                    return products_
+            return False
+        def save(products_):
+            with open('products.json', 'w', encoding='utf-8') as file:
+                json.dump(products_, file, ensure_ascii=False, indent=4)
+        products = edit(load())
+        if products:
+            save(products)
+            return True
+        else:
+            return False
+
+    def check_availability(self, number_product):
+        if number_product == 1:
+            pizza = Pepperoni()
+            ingredients = pizza.ingredients
+            for _ in ingredients:
+                if self.subtract_products(_, ingredients[_]) == False:
+                    return False
+        elif number_product == 2:
+            pizza = Margarita()
+            ingredients = pizza.ingredients
+            for _ in ingredients:
+                if self.subtract_products(_, ingredients[_]) == False:
+                    return False
+        elif number_product == 3:
+            pizza = FourCheeses()
+            ingredients = pizza.ingredients
+            for _ in ingredients:
+                if self.subtract_products(_, ingredients[_]) == False:
+                    return False
+        elif number_product == 4:
+            pizza = HamCheese()
+            ingredients = pizza.ingredients
+            for _ in ingredients:
+                if self.subtract_products(_, ingredients[_]) == False:
+                    return False
+        elif number_product == 5:
+            pizza = Hawaiian()
+            ingredients = pizza.ingredients
+            for _ in ingredients:
+                if self.subtract_products(_, ingredients[_]) == False:
+                    return False
+        return True
