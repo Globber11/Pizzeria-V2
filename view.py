a51480a96322ea3cacd686cff6efb69ca13496ac
ViperrIvan <smokerskatedik@gmail.com>
2025-04-16 22:39:54 +0700

    Update view.py

--- view.py
+++ view.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import Dict
 
 class OrderView:
     @classmethod
-    def product_selection(cls, product_names: List):
+    def product_selection(cls, product_names: Dict):
         print("Выберите продукт:")
         for _ in range(1,len(product_names)+1):
-            print(f"\t{_}. {product_names[_-1]}")
+            print(f"\t{_}. {product_names[str(_)]}")
 
 
 class CheckGenerator:
