7e15f2cccb359c147cff0639e17f1737c64dfbb7
Globber11 <kg05052011@gmail.com>
2025-04-16 21:15:30 +0700

    Update view.py

--- view.py
+++ view.py
@@ -1,17 +1,6 @@
-from model import reg
-from controller import dc
-class RegistrationView:
-    def crate_account(self):
-        print('Приветствую тебя во второй версии пиццерии!\nДля начала тебе необходимо пройти регистрацию/авторизацию!')
-        name = input('Введите ваше имя:')
-        surname = input('Введите вашу фамилию:')
-        phone_number = int(input('Введите ван номер телефона без + и пробелов:'))
-        while True:
-            try:
-                born_date = input('Введите вашу дату рождения:')
-                if not dc.check_date(born_date):
-                    raise ValueError
-                break
-            except:
-                print('Дата введена неверно, попробуйте снова')
-        reg.crate_account(name, surname, phone_number, born_date)
+class CheckGenerator:
+    def __init__(self, busket: dict[str, list[int, int]]):
+        self.__busket = busket
+    def print_check(self):
+        print("\n" + "=" * 30)
+        for _ in self.__busket:
