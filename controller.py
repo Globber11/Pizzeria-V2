1c57d98eb5380d52182d95b895e6369cea95276f
Globber11 <kg05052011@gmail.com>
2025-04-16 21:16:08 +0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,9 +1,26 @@
 import re
+from model import reg
 
 class DataChecker:
     def check_date(self, date):
         if re.match(r'^(?:(?:31\.(?:0[13578]|1[02])|(?:29|30)\.(?:0[13-9]|1[0-2]))\.\d{4}|(?:0[1-9]|1\d|2[0-8])\.(?:0[1-9]|1[0-2])\.\d{4}|29\.02\.(?:\d{2}(?:0[48]|[2468][048]|[13579][26])|(?:[02468][048]|[13579][26])00))$', date):
             return True
         return False
+
+class RegistrationView:
+    def crate_account(self):
+        print('Приветствую тебя во второй версии пиццерии!\nДля начала тебе необходимо пройти регистрацию/авторизацию!')
+        name = input('Введите ваше имя:')
+        surname = input('Введите вашу фамилию:')
+        phone_number = int(input('Введите ван номер телефона без + и пробелов:'))
+        while True:
+            try:
+                born_date = input('Введите вашу дату рождения:')
+                if not dc.check_date(born_date):
+                    raise ValueError
+                break
+            except:
+                print('Дата введена неверно, попробуйте снова')
+        reg.crate_account(name, surname, phone_number, born_date)
 
 dc = DataChecker()
