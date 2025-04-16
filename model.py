8e66f5ebcc7c9c456385ea86cfd30b191806be23
Globber11 <kg05052011@gmail.com>
2025-04-16 21:14:59 +0700

    Update model.py

--- model.py
+++ model.py
@@ -126,13 +126,6 @@ class Busket:
 
     def get_busket(self):
         return self.__busket
-
-class CheckGenerator:
-    def __init__(self, busket: dict[str, list[int, int]]):
-        self.__busket = busket
-    def print_check(self):
-        print("\n" + "=" * 30)
-        for _ in self.__busket:
             print(f'{_} {self.__busket[_][1]}шт.')
         print(f'Total to pay: {sum(self.__busket[_][0] * self.__busket[_][1] for _ in self.__busket)}')
         print("=" * 30)
